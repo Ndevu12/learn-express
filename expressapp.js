3d6f07fd6476c625013959d0ab2786cf2c12b946
Jean Paul Elisa NIYOKWIZERWA <niyokwizerwajeanpaulelisa@gmail.com>
2026-05-07 20:20:19 +0200

    docs: update documentation to reflect new folder structure

--- expressapp.js
+++ expressapp.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const userRouters = require('./userRoutes');
-const cors = require("cors");
 
 const app = express();
 
 // Express exposes all http methods for you automatically as a collable class methods
+/*
+* The first parameter of the method is the route, and the second parameter is the callback function that will be executed when the route is hit.
+* The callback function takes two parameters, the request and the response objects.
+* The request object contains information about the incoming request, such as the URL, headers, and body.
+* The response object is used to send a response back to the client.
+*/
 app.get("/", (req, res) => {
 res.send("Hello home");
 })
 
-app.post("/post", (r, p) => {
+// Honestly, you can name the request and response parameters as you want, 
+// but these (req, and res) are the most common and taken as the standard ones
+/*
+* Let us name those parameters as r and p, and see if it works.
+* Yes, it works, but it is not recommended to do so, 
+* as it can cause confusion for other developers who are used to the standard naming convention.
+*/
+app.post("/", (r, p) => {
     p.send("This is a post.")
 })
-
-app.use("/users", userRouters)
 
 
 const port = 5000;
